3a4ce16165f9ca5eaaa4133220758c5b49ca8b8e
Nicolás Florentín <nicogrunger@hotmail.com>
2021-08-17 23:00:58 -0300

    cambios para entrega

---
+++ src/NavBar.js
@@ -0,0 +1,13 @@
+import React from "react"
+import ReactDom from "react-dom"
+
+const NavBar = () => {
+	return (
+		<nav className = "navBar">
+			<ul><button>Modulos</button></ul>
+			<ul><button>Precios</button></ul>
+			<ul><button>Nosotros</button></ul>
+		</nav>
+)}
+
+export default NavBar
